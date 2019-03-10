60331133329a379c1c767fc221300926926d2812
Sharla Gelfand <sharla.gelfand@gmail.com>
2019-03-10 11:22:14 -0500

    Add raw data file, code refactor

---
+++ R/get_unconf_issues.R
@@ -0,0 +1,52 @@
+#' Keep issues and columns
+#' 
+#' Only keep issues (no PRs) and relevant columns from pulled issues
+#'
+#' @param df 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+keep_issues_and_cols <- function(df) {
+  df %>%
+    dplyr::as_tibble() %>%
+    dplyr::filter(stringr::str_detect(url, "issues")) %>%
+    dplyr::select(number, title, user_login, state, url,
+                  created_at, closed_at, body, labels_name)
+}
+
+#' Get labelled issues from a specific repository
+#'
+#' @param repo_owner 
+#' @param repo_name 
+#' @param label 
+#' @param ... 
+#'
+#' @export
+get_labelled_issues <- function(repo_owner, repo_name, label, ...){
+  projmgr::get_issues(
+    projmgr::create_repo_ref(repo_owner, repo_name),
+    labels = label,
+    state = "all"
+  ) %>%
+    projmgr::parse_issues() %>%
+    keep_issues_and_cols() %>%
+    dplyr::mutate(repo_owner = repo_owner,
+                  repo_name = repo_name,
+                  label = label)
+}
+
+#' Get all unconf issues from specified repositories
+#'
+#' @param repositories 
+#' @param issue_type
+#'
+#' @export
+get_repos_issues <- function(repositories_path, issue_type){
+  repositories <- readr::read_csv(here::here(repositories_path), na = "NA")
+  
+  repositories %>% 
+    purrr::pmap_dfr(get_labelled_issues) %>%
+    dplyr::filter(state == issue_type)
+}
